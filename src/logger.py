337238e18978c88fe7f7ac19cc0f3403010d2700
zisling <hedi120@gmail.com>
2020-11-19 13:20:17 +0200

    try to convert

--- src/logger.py
+++ src/logger.py
@@ -6,20 +6,18 @@ Licensed under the CC BY-NC-ND 4.0 license (https://creativecommons.org/licenses
 import tensorflow as tf
 import numpy as np
 import scipy.misc
-
-try:
-    from StringIO import StringIO  # Python 2.7
-except ImportError:
-    from io import BytesIO  # Python 3.x
+from PIL import Image
+from io import BytesIO  # Python 3.x
 
 
 class Logger(object):
     def __init__(self, log_dir, suffix=None):
-        self.writer = tf.summary.FileWriter(log_dir, filename_suffix=suffix)
+        self.writer = tf.summary.create_file_writer(log_dir, filename_suffix=suffix)
 
     def scalar_summary(self, tag, value, step):
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
 
     def image_summary(self, tag, images, step):
 
@@ -30,7 +28,7 @@ class Logger(object):
                 s = StringIO()
             except:
                 s = BytesIO()
-            scipy.misc.toimage(img).save(s, format="png")
+            Image.fromarray(img).save(s, format="png")
 
             # Create an Image object
             img_sum = tf.Summary.Image(encoded_image_string=s.getvalue(),
@@ -64,7 +62,7 @@ class Logger(object):
             v = [np.squeeze(f) for f in np.split(v, v.shape[0], axis=0)]
             img = np.concatenate(v, axis=1)[:, :-1, :]
 
-            scipy.misc.toimage(img).save(s, format="png")
+            Image.fromarray(img).save(s, format="png")
 
             # Create an Image object
             img_sum = tf.Summary.Image(encoded_image_string=s.getvalue(),
