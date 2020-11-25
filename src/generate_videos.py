328d9b7138f616618538c9751944f8bd9625b6e7
zisling <hedi120@gmail.com>
2020-11-25 18:04:05 +0200

    add new generate_videos

--- src/generate_videos.py
+++ src/generate_videos.py
@@ -1,63 +1,46 @@
 """
-Copyright (C) 2017 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-ND 4.0 license (https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode).
-
-Sergey Tulyakov, Ming-Yu Liu, Xiaodong Yang, Jan Kautz, MoCoGAN: Decomposing Motion and Content for Video Generation
-https://arxiv.org/abs/1707.04993
-
-Generates multiple videos given a model and saves them as video files using ffmpeg
-
 Usage:
-    generate_videos.py [options] <model> <output_folder>
+    generate_videos.py --model_path=<path> --out_path=<path>
 
-Options:
-    -n, --num_videos=<count>                number of videos to generate [default: 10]
-    -o, --output_format=<ext>               save videos as [default: gif]
-    -f, --number_of_frames=<count>          generate videos with that many frames [default: 16]
-
-    --ffmpeg=<str>                          ffmpeg executable (on windows should be ffmpeg.exe). Make sure
-                                            the executable is in your PATH [default: ffmpeg]
 """
 
 import os
-import docopt
 import torch
 
 from trainers import videos_to_numpy
 
-import subprocess as sp
+import docopt
+import numpy as np
+import imageio
 
 
-def save_video(ffmpeg, video, filename):
-    command = [ffmpeg,
-               '-y',
-               '-f', 'rawvideo',
-               '-vcodec', 'rawvideo',
-               '-s', '64x64',
-               '-pix_fmt', 'rgb24',
-               '-r', '8',
-               '-i', '-',
-               '-c:v', 'mjpeg',
-               '-q:v', '3',
-               '-an',
-               filename]
-
-    pipe = sp.Popen(command, stdin=sp.PIPE, stderr=sp.PIPE)
-    pipe.stdin.write(video.tostring())
+def crate_gif(video, filename):
+    imageio.mimsave(filename, video)
 
 
 if __name__ == "__main__":
+    print('Setting up video generating program')
     args = docopt.docopt(__doc__)
+    print(args)
 
-    generator = torch.load(args["<model>"], map_location={'cuda:0': 'cpu'})
+    model_path = args['--model_path']
+    out_path = args['--out_path']
+    generator = torch.load(model_path)
+    print('Loaded model')
     generator.eval()
-    num_videos = int(args['--num_videos'])
-    output_folder = args['<output_folder>']
+    print('Evaluated generator')
+    num_videos = 10
 
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
 
+    print('Starting generation')
     for i in range(num_videos):
-        v, _ = generator.sample_videos(1, int(args['--number_of_frames']))
+        v, _ = generator.sample_videos(1, 35 * 2)
         video = videos_to_numpy(v).squeeze().transpose((1, 2, 3, 0))
-        save_video(args["--ffmpeg"], video, os.path.join(output_folder, "{}.{}".format(i, args['--output_format'])))
+        print(video.shape)
+        crate_gif(video[:, :, :, 0:3], os.path.join(out_path, "{}_objs.{}".format(i, "gif")))
+        crate_gif(video[:, :, :, 3:6], os.path.join(out_path, "{}_background.{}".format(i, "gif")))
+        crate_gif(video[:, :, :, 6], os.path.join(out_path, "{}_depth.{}".format(i, "gif")))
+        print('finished gif no. {}'.format(i))
+    print('Finished generation')
